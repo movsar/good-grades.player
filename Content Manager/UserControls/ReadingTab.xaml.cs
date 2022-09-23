afa9c7cf1171c15f5a64e8fbec7d68ed478207f7
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-09-23 21:49:39 +0300

    savepoint

---
+++ Content Manager/UserControls/ReadingTab.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Content_Manager.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ReadingTab.xaml
+    /// </summary>
+    public partial class ReadingTab : UserControl
+    {
+        public ReadingTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
