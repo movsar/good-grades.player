d0d15107e14f2bbcec94fc9d6a2af907d1562758
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-09-27 05:53:26 +0300

    savepoint

--- Content Manager/UserControls/ListeningMaterialControl.xaml.cs
+++ Content Manager/UserControls/ListeningMaterialControl.xaml.cs
@@ -1,28 +1,12 @@
-﻿using Content_Manager.Services;
-using Content_Manager.Stores;
-using Content_Manager.Windows;
+﻿using Content_Manager.Stores;
 using Data.Models;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Content_Manager.UserControls
 {
-    /// <summary>
-    /// Interaction logic for ListeningMaterialControl.xaml
-    /// </summary>
     public partial class ListeningMaterialControl : UserControl
     {
 
---
+++ Content Manager/Windows/ListeningPreviewWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Content_Manager.Windows
+{
+    /// <summary>
+    /// Interaction logic for ListeningPreviewWindow.xaml
+    /// </summary>
+    public partial class ListeningPreviewWindow : Window
+    {
+        public ListeningPreviewWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
