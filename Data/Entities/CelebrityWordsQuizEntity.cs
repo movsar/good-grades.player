3cd4e6cfa82b8eca28b7a6a0324b5069e2414beb
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-10-19 15:03:00 +0300

    wrote a nice generic method to handle them all

--- Data/Entities/CelebrityWordsQuizEntity.cs
+++ Data/Entities/CelebrityWordsQuizEntity.cs
@@ -1,7 +1,9 @@
 ﻿using Data.Interfaces;
 using Data.Models;
+using Data.Services;
 using MongoDB.Bson;
 using Realms;
+using System.Collections.Generic;
 
 namespace Data.Entities {
     public class CelebrityWordsQuizEntity : RealmObject, IEntityBase, ICelebrityWordsQuiz {
@@ -23,35 +25,8 @@ namespace Data.Entities {
         public void SetFromModel(IModelBase model) {
             var celebrityWordsQuiz = model as CelebrityWordsQuiz;
             SegmentId = celebrityWordsQuiz!.SegmentId;
-            
-            SetCwqOptions(celebrityWordsQuiz);
-        }
 
-        private void SetCwqOptions(CelebrityWordsQuiz cwq) {
-            if (cwq.Options == null) {
-                return;
-            }
-
-            // Commit removed options
-            var currentOptionIds = cwq.Options.Select(x => x.Id).ToList();
-            var optionsToRemove = Options.Where(rm => !currentOptionIds.Contains(rm.Id));
-            foreach (var option in optionsToRemove) {
-                Options.Remove(option);
-            }
-
-            // Add or update options
-            foreach (var option in cwq.Options) {
-                var existingOption = Options?.FirstOrDefault((rm => rm.Id == option.Id));
-                if (existingOption != null) {
-                    existingOption.SetFromModel(option);
-                } else {
-                    var newOption = new CwqOptionEntity();
-                    newOption.SetFromModel(option);
-                    Options?.Add(newOption);
-
-                    option.Id = newOption.Id;
-                }
-            }
+            Utils.SyncLists(Options, celebrityWordsQuiz.Options);
         }
         #endregion
     }
---
+++ Data/Services/Utils.cs
@@ -0,0 +1,51 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Services {
+    internal static class Utils {
+        internal static void SyncLists<TEntity, TModel>(IList<TEntity> entities, List<TModel> models)
+            where TEntity : IEntityBase, new()
+            where TModel : IModelBase, new() {
+
+            // This method is used to synchronize list of items changed on a model, with that of an entity
+            // It checks whether something was removed, updated or added, if added, it also sets items' ids
+
+            if (models == null || entities == null) {
+                return;
+            }
+
+            // Existing elemenet ids
+            var currentItemIds = models.Select(x => x.Id).ToList();
+
+            // Models that have been removed
+            var itemsToRemove = entities.Where(rm => !currentItemIds.Contains(rm.Id));
+
+            // Remove corresponding to removed models entities
+            foreach (var option in itemsToRemove) {
+                entities.Remove(option);
+            }
+
+            // Add or update
+            foreach (var item in models) {
+                var existingItem = entities.FirstOrDefault((rm => rm.Id == item.Id));
+                if (existingItem != null) {
+                    existingItem.SetFromModel(item);
+                } else {
+                    var newItem = new TEntity();
+                    newItem.SetFromModel(item);
+                    entities?.Add(newItem);
+
+                    // Set item id
+                    item.Id = newItem.Id;
+                }
+            }
+        }
+
+    }
+}
