eea598241f325b4ee7a07e872bdeec68966f8f01
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-10-20 22:59:18 +0300

    savepoint

--- Content Manager/Windows/RtbPreviewWindow.xaml.cs
+++ Content Manager/Windows/RtbPreviewWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Text;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace Content_Manager.Windows {
     public partial class RtbPreviewWindow : Window {
@@ -10,9 +11,8 @@ namespace Content_Manager.Windows {
             InitializeComponent();
             MemoryStream stream = new MemoryStream(ASCIIEncoding.Default.GetBytes(content));
 
-
-        //    rtbMain.Selection.Load(stream, DataFormats.Rtf);
-        //    rtbMain.Selection.Select(rtbMain.Document.ContentEnd, rtbMain.Document.ContentEnd);
+            TextRange textRange = new TextRange(flowDocument.ContentStart, flowDocument.ContentEnd);
+            textRange.Load(stream, DataFormats.Xaml);
         }
     }
 }
