bbc402b085f98aea27e11ed74bcaa85bc6454e71
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-10-23 11:11:26 +0300

    added a new quizitem

---
+++ Data/Entities/TestingQuizItemEntity.cs
@@ -0,0 +1,38 @@
+﻿using Data.Interfaces;
+using Data.Models;
+using MongoDB.Bson;
+using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class TestingQuizItemEntity : RealmObject, ITestingQuizItem, IEntityBase
+    {
+        [Required]
+        [PrimaryKey]
+        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public IList<string> Options { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset ModifiedAt { get; set; } = DateTimeOffset.Now;
+
+        public IModelBase ToModel()
+        {
+            return new TestingQuizItem(this);
+        }
+
+        public void SetFromModel(IModelBase model)
+        {
+            var quizItem = model as TestingQuizItem;
+            Question = quizItem.Question;
+            Answer = quizItem.Answer;
+            Options = quizItem.Options;
+        }
+    }
+}
---
+++ Data/Interfaces/ITestingQuizItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface ITestingQuizItem : IModelBase
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public IList<string> Options { get; set; }
+    }
+}
