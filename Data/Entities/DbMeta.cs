834c871bbec4e5dcc3cb47ba09502025526f3265
Movsar Bekaev <movsar.dev@gmail.com>
2023-12-27 08:36:23 +0300

    save

--- Data/Entities/DbMeta.cs
+++ Data/Entities/DbMeta.cs
@@ -6,16 +6,13 @@ namespace Data.Entities
 {
     public class DbMeta : RealmObject, IEntityBase
     {
-        #region Properties
-        [Required]
-        [PrimaryKey]
-        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+        [PrimaryKey] public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
         public DateTimeOffset CreatedAt { get; } = DateTimeOffset.Now;
         public DateTimeOffset ModifiedAt { get; set; } = DateTimeOffset.Now;
+        /******************************************************************/
         public string Title { get; set; }
         public string Description { get; set; }
         public string? AppVersion { get; set; }
-        #endregion
 
     }
 }
