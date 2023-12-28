2ec2aafc24b1331ee031c77818f67bf11745a850
Movsar Bekaev <movsar.dev@gmail.com>
2023-12-28 15:08:23 +0300

    added task type user control

---
+++ Content Manager/UserControls/MaterialControls/TaskMaterialControl.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Content_Manager.UserControls.MaterialControls
+{
+    /// <summary>
+    /// Interaction logic for TaskMaterialControl.xaml
+    /// </summary>
+    public partial class TaskMaterialControl : UserControl
+    {
+        public TaskMaterialControl()
+        {
+            InitializeComponent();
+
+            var fillingTaskType = new ComboBoxItem()
+            {
+                Content = "Заполнение пробелов"
+            };
+
+            var selectingTaskType = new ComboBoxItem()
+            {
+                Content = "Выбор правильного"
+            };
+
+            var testingTaskType = new ComboBoxItem()
+            {
+                Content = "Тест"
+            };
+
+            var matchingTaskType = new ComboBoxItem()
+            {
+                Content = "Сопоставление"
+            };
+
+            var buildingTaskMaterial = new ComboBoxItem()
+            {
+                Content = "Построение выражений"
+            };
+
+            cmbTaskType.Items.Add(fillingTaskType);
+            cmbTaskType.Items.Add(selectingTaskType);
+            cmbTaskType.Items.Add(testingTaskType);
+            cmbTaskType.Items.Add(matchingTaskType);
+            cmbTaskType.Items.Add(buildingTaskMaterial);
+        }
+
+        private void btnSetData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPreview_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmbTaskType_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmbTaskType_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmbTaskType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
--- Content Manager/UserControls/SegmentTabs/TasksTab.xaml.cs
+++ Content Manager/UserControls/SegmentTabs/TasksTab.xaml.cs
@@ -1,12 +1,45 @@
-﻿using System.Windows.Controls;
+﻿using Content_Manager.Stores;
+using Content_Manager.UserControls.MaterialControls;
+using Data.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
 
 namespace Content_Manager.UserControls.SegmentTabs
 {
     public partial class TasksTab : UserControl
     {
+        private ContentStore ContentStore => App.AppHost!.Services.GetRequiredService<ContentStore>();
         public TasksTab()
         {
             InitializeComponent();
+            DataContext = this;
+
+            ContentStore.ItemAdded += ContentStore_ItemChanged;
+            ContentStore.ItemDeleted += ContentStore_ItemChanged;
+            ContentStore.ItemUpdated += ContentStore_ItemChanged;
+
+            RedrawUi();
+        }
+        public void RedrawUi()
+        {
+            spTaskMaterialControls.Children.Clear();
+
+            if (ContentStore.SelectedSegment!.ListeningMaterials == null) return;
+
+            foreach (var material in ContentStore.SelectedSegment!.ListeningMaterials)
+            {
+                //var existingMaterial = new TaskMaterialControl(material);
+                //spTaskMaterialControls.Children.Add(existingMaterial);
+            }
+
+            var newMaterial = new TaskMaterialControl();
+
+            spTaskMaterialControls.Children.Add(newMaterial);
+        }
+
+        private void ContentStore_ItemChanged(IEntityBase entity)
+        {
+            RedrawUi();
         }
     }
 }
