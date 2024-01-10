05488370da04b711528920e4a7c01e172e50fd63
Movsar Bekaev <movsar.dev@gmail.com>
2024-01-10 16:47:21 +0300

    building works

--- SharedControls/Viewers/BuildingViewer.xaml.cs
+++ SharedControls/Viewers/BuildingViewer.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Data.Entities;
 using Data.Entities.TaskItems;
 using Shared.Controls;
+using Shared.Translations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,13 @@ namespace Shared.Viewers
     public partial class BuildingViewer : Window
     {
         IList<AssignmentItem> _items;
+        public string TaskTitle { get; }
         public BuildingViewer(BuildingTaskAssignment taskAssignment)
         {
             InitializeComponent();
             DataContext = this;
 
+            TaskTitle = taskAssignment.Title;
             _items = taskAssignment.Items; _items = taskAssignment.Items;
 
             foreach (var item in _items)
@@ -24,6 +27,21 @@ namespace Shared.Viewers
                 var buildingItemViewControl = new BuildingItemViewControl(item.Text);
                 spItems.Children.Add(buildingItemViewControl);
             }
+        }
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (BuildingItemViewControl buildingItemViewControl in spItems.Children)
+            {
+                // Check if the user input matches one of the options
+                if (!buildingItemViewControl.IsCorrectlyArranged)
+                {
+                    MessageBox.Show(Ru.Incorrect);
+                    return;
+                }
+            }
+
+            // Show a message if all inputs are correct
+            MessageBox.Show(Ru.Correct);
         }
     }
 }
