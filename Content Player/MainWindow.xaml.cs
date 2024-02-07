e51a55184cbeeaa3a6ef9cd0c3e95a64c87e90cb
Movsar Bekaev <movsar.dev@gmail.com>
2024-02-07 15:20:16 +0300

    save

--- Content Player/MainWindow.xaml.cs
+++ Content Player/MainWindow.xaml.cs
@@ -1,14 +1,15 @@
 ﻿using Data;
 using Data.Entities;
-using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Content_Player
 {
     public partial class MainWindow : Window
     {
         public List<Segment> Segments { get; }
+        public string DbTitle { get; }
         public MainWindow(Storage storage)
         {
             InitializeComponent();
@@ -21,6 +22,30 @@ namespace Content_Player
             storage.SetDatabaseConfig(dbAbsolutePath);
             var db = storage.Database;
             Segments = db.All<Segment>().ToList();
+            var meta = db.All<DbMeta>().First();
+            DbTitle = meta.Title;
         }
+        private void LoadSegment()
+        {
+            var selectedSegment = (Segment)lvSegments.SelectedItem;
+            if (selectedSegment == null)
+            {
+                return;
+            }
+        }
+
+        #region Event handlers
+        private void lvSegments_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            LoadSegment();
+        }
+        private void lvSegments_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return || e.Key == Key.Enter)
+            {
+                LoadSegment();
+            }
+        }
+        #endregion
     }
 }
