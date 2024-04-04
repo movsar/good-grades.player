022d39417364e3a2331a3d59307a4c35dd19109c
Movsar Bekaev <movsar.dev@gmail.com>
2024-04-04 21:50:08 +0300

    building works

--- SharedControls/Controls/BuildingItemViewControl.xaml.cs
+++ SharedControls/Controls/BuildingItemViewControl.xaml.cs
@@ -1,31 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Data.Entities.TaskItems;
+using System;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace Shared.Controls
 {
     public partial class BuildingItemViewControl : UserControl
     {
-        // Property to indicate if the arrangement is correct
-        public bool IsCorrectlyArranged { get; set; } = false;
-
-        // The original phrase for comparison
-        private string _originalPhrase;
-        private List<string> _shuffledWords = new List<string>();
-
-        public BuildingItemViewControl(string phrase)
+        public BuildingItemViewControl(AssignmentItem item)
         {
             InitializeComponent();
             DataContext = this;
 
-            _originalPhrase = phrase;
-
             // Shuffle words and add them as buttons
-            _shuffledWords = phrase.Split(" ").OrderBy(w => Guid.NewGuid()).ToList();
-            foreach (string word in _shuffledWords)
+            var shuffledWords = item.Text.Split(" ").OrderBy(w => Guid.NewGuid()).ToList();
+            foreach (string word in shuffledWords)
             {
                 var btnWord = new Button()
                 {
@@ -35,62 +25,25 @@ namespace Shared.Controls
                 };
 
                 // Event handlers for drag and drop
-                btnWord.PreviewMouseLeftButtonDown += BtnWord_MouseLeftButtonDown;
-                btnWord.Drop += BtnWord_Drop;
+                btnWord.Click += BtnWord_Click;
 
-                spItems.Children.Add(btnWord);
+                spItemSource.Children.Add(btnWord);
             }
         }
 
-        private void BtnWord_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void BtnWord_Click(object sender, RoutedEventArgs e)
         {
-            // Begin drag operation
             Button btnWord = sender as Button;
-            DragDrop.DoDragDrop(btnWord, btnWord.Content, DragDropEffects.Move);
-        }
-
-        private void BtnWord_Drop(object sender, DragEventArgs e)
-        {
-            // Handle drop operation
-            Button target = sender as Button;
-            string sourceWord = e.Data.GetData(DataFormats.StringFormat) as string;
-            int targetIndex = _shuffledWords.IndexOf(target.Content as string);
-            int sourceIndex = _shuffledWords.IndexOf(sourceWord);
-
-            if (sourceIndex < 0 || targetIndex < 0) {
-                return;
-            }
-            // Swap words
-            _shuffledWords[targetIndex] = sourceWord;
-            _shuffledWords[sourceIndex] = target.Content as string;
-
-            // Update UI
-            spItems.Children.Clear();
-            foreach (string word in _shuffledWords)
+            if (spItemSource.Children.Contains(btnWord))
             {
-                var btnWord = new Button()
-                {
-                    Content = word,
-                    AllowDrop = true,
-                    Style = (Style)FindResource("BuilderItemButtonStyle"),
-                };
-
-                btnWord.PreviewMouseLeftButtonDown += BtnWord_MouseLeftButtonDown;
-                btnWord.Drop += BtnWord_Drop;
-
-                spItems.Children.Add(btnWord);
+                spItemSource.Children.Remove(btnWord);
+                spItemDropZone.Children.Add(btnWord);
             }
-
-            // Check if correctly arranged
-            CheckArrangement();
-        }
-
-        private void CheckArrangement()
-        {
-            // Check if the current arrangement matches the original phrase
-            IsCorrectlyArranged = string.Join(" ", _shuffledWords) == _originalPhrase;
-
-            // You can add more logic here to notify about the correct arrangement
+            else
+            {
+                spItemDropZone.Children.Remove(btnWord);
+                spItemSource.Children.Add(btnWord);
+            }
         }
     }
 }
--- SharedControls/Viewers/BuildingViewer.xaml.cs
+++ SharedControls/Viewers/BuildingViewer.xaml.cs
@@ -14,26 +14,23 @@ namespace Shared.Viewers
 {
     public partial class BuildingViewer : Window, IAssignmentViewer
     {
-        IList<AssignmentItem> _items;
-
         public event Action<IAssignment, bool> CompletionStateChanged;
 
         private readonly BuildingAssignment _assignment;
 
         public string TaskTitle { get; }
-        public BuildingViewer(BuildingAssignment taskAssignment)
+        public BuildingViewer(BuildingAssignment assignment)
         {
             InitializeComponent();
             DataContext = this;
 
-            _assignment = taskAssignment;
+            _assignment = assignment;
 
             TaskTitle = _assignment.Title;
-            _items = _assignment.Items;
 
-            foreach (var item in _items)
+            foreach (var item in _assignment.Items)
             {
-                var buildingItemViewControl = new BuildingItemViewControl(item.Text);
+                var buildingItemViewControl = new BuildingItemViewControl(item) { Tag = item.Text };
                 spItems.Children.Add(buildingItemViewControl);
             }
         }
@@ -41,8 +38,10 @@ namespace Shared.Viewers
         {
             foreach (BuildingItemViewControl buildingItemViewControl in spItems.Children)
             {
+                var arrangedPhrase = GetUserArrangedPhrase(buildingItemViewControl);
+
                 // Check if the user input matches one of the options
-                if (!buildingItemViewControl.IsCorrectlyArranged)
+                if (arrangedPhrase != buildingItemViewControl.Tag.ToString())
                 {
                     CompletionStateChanged?.Invoke(_assignment, false);
                     MessageBox.Show(Ru.Incorrect);
@@ -53,6 +52,17 @@ namespace Shared.Viewers
             // Show a message if all inputs are correct
             CompletionStateChanged?.Invoke(_assignment, true);
             MessageBox.Show(Ru.Correct);
+        }
+
+        private string GetUserArrangedPhrase(BuildingItemViewControl buildingItemViewControl)
+        {
+            var words = new List<string>();
+            foreach (Button btnWord in buildingItemViewControl.spItemDropZone.Children)
+            {
+                words.Add(btnWord.Content.ToString());
+            }
+
+            return string.Join(" ", words);
         }
     }
 }
