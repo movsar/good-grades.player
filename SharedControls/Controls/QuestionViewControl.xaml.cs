c0192c03d4b763d0657163b9b10d55e1266be0a8
Movsar Bekaev <movsar.dev@gmail.com>
2024-04-04 11:50:47 +0300

    added variating uis for multichoice and single choice options

--- SharedControls/Controls/QuestionViewControl.xaml.cs
+++ SharedControls/Controls/QuestionViewControl.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Data.Entities.TaskItems;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Shared.Controls
@@ -14,7 +16,55 @@ namespace Shared.Controls
             DataContext = this;
 
             Question = testingQuestion;
+
+            var isMultichoice = Question.Options.Where(o => o.IsChecked).Count() > 1;
+            spOptions.Children.Clear();
+            foreach (var option in Question.Options)
+            {
+                var spOption = isMultichoice ? GenerateCheckboxOptionView(option) : GenerateRadioButtonOptionView(option, Question.Id);
+                spOptions.Children.Add(spOption);
+            }
         }
+
+        private StackPanel GenerateRadioButtonOptionView(AssignmentItem option, string groupName)
+        {
+            var spOption = new StackPanel()
+            {
+                Tag = option.Id,
+                Orientation = Orientation.Horizontal
+            };
+
+            var radioButton = new RadioButton()
+            {
+                GroupName = groupName,
+                Content = option.Text,
+                Style = (Style)FindResource("RadioOptionStyle"),
+            };
+
+            spOption.Children.Add(radioButton);
+
+            return spOption;
+        }
+
+        private StackPanel GenerateCheckboxOptionView(AssignmentItem option)
+        {
+            var spOption = new StackPanel()
+            {
+                Tag = option.Id,
+                Orientation = Orientation.Horizontal
+            };
+
+            var checkbox = new CheckBox()
+            {
+                Content = option.Text,
+                Style = (Style)FindResource("CheckboxOptionStyle"),
+            };
+
+            spOption.Children.Add(checkbox);
+
+            return spOption;
+        }
+
 
         private void RadioButton_Checked(object sender, System.Windows.RoutedEventArgs e)
         {
