71dcdd1a621025ad9c618850ef86577a7082a5e1
Movsar Bekaev <movsar.dev@gmail.com>
2024-04-05 19:36:51 +0300

    added vpk to gg player

---
+++ GGPlayer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Velopack;
+
+namespace GGPlayer
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            VelopackApp.Build().Run();
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
