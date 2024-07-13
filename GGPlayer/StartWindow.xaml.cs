89d2d98ab2d793658656b6792190cc99b1fec175
Movsar Bekaev <movsar.dev@gmail.com>
2024-07-13 08:57:42 +0300

    removed unused usings

--- GGPlayer/StartWindow.xaml.cs
+++ GGPlayer/StartWindow.xaml.cs
@@ -1,31 +1,15 @@
 ﻿
 using Data;
-using Data.Entities;
 using Data.Services;
-using GGManager;
 using GGManager.Stores;
-using GGPlayer.Pages;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Win32;
 using Shared;
 using Shared.Services;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace GGPlayer
 {
