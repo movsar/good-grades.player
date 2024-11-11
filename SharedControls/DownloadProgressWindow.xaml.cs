d1900d6e1eafe496dabead33c79765cde9f5c18f
Movsar Bekaev <movsar.dev@gmail.com>
2024-11-11 09:20:47 +0300

    save

---
+++ SharedControls/DownloadProgressWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Shared
+{
+    /// <summary>
+    /// Interaction logic for DownloadProgressWindow.xaml
+    /// </summary>
+    public partial class DownloadProgressWindow : Window
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public DownloadProgressWindow(CancellationTokenSource cancellationTokenSource)
+        {
+            InitializeComponent();
+            _cancellationTokenSource = cancellationTokenSource;
+        }
+
+        public void UpdateProgress(double progress)
+        {
+            DownloadProgressBar.Value = progress;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
+            Close();
+        }
+    }
+}
