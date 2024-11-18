1c474aaa5f8ea001b8597e5039d72e751cc9a369
FraintSaad <islamqwertyu@mail.ru>
2024-11-18 23:22:41 +0300

    fix exception for empty fields in test

--- SharedControls/Services/TextService.cs
+++ SharedControls/Services/TextService.cs
@@ -4,6 +4,10 @@
     {
         public static string GetChechenString(string rawStr)
         {
+            if (string.IsNullOrWhiteSpace(rawStr))
+            {
+                return string.Empty;
+            }
             return rawStr.Replace("1", "Ӏ")
                 .Replace("I", "Ӏ")
                 .Trim();
