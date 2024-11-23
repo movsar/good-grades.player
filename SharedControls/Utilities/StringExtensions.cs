0e82566e82227d4bb223c7ed61f953fd7be7ca88
FraintSaad <islamqwertyu@mail.ru>
2024-11-23 19:14:26 +0300

    Added assignments name under ComboBox

---
+++ SharedControls/Utilities/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string value, int maxChars)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxChars ? value : value.Substring(0, maxChars) + "...";
+        }
+    }
+}
