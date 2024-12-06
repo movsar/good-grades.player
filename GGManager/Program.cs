ad831b61831eeb85b18fc716b9f50b2f34791d81
FraintSaad <islamqwertyu@mail.ru>
2024-12-06 23:02:25 +0300

    Upgraded and changed path in script

--- GGManager/Program.cs
+++ GGManager/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Serilog;
 using Shared.Services;
+using Shared.Utilities;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -13,9 +14,9 @@ namespace GGManager
         [STAThread]
         public static void Main(string[] args)
         {
-            if (IsWindows7())
+            if (FontInstaller.IsWindows7())
             {
-                RunFontInstallScript();
+                FontInstaller.RunFontInstallScript();
             }
             string logPath = Path.Combine("logs", "logs.txt");
             Log.Logger = new LoggerConfiguration()
@@ -28,38 +29,6 @@ namespace GGManager
             var application = new App();
             application.InitializeComponent();
             application.Run();
-        }
-
-
-        private static bool IsWindows7()
-        {
-            return Environment.OSVersion.Platform == PlatformID.Win32NT &&
-                   Environment.OSVersion.Version.Major == 6 &&
-                   Environment.OSVersion.Version.Minor == 1;
-        }
-
-        private static void RunFontInstallScript()
-        {
-            string scriptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "Fonts", "InstallFont.bat");
-
-            if (!File.Exists(scriptPath))
-            {
-                return;
-            }
-
-            try
-            {
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = scriptPath,
-                    Verb = "runas",
-                    UseShellExecute = true
-                })?.WaitForExit();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"An error occurred while running the script: {ex.Message}");
-            }
         }
 
         public static Microsoft.Extensions.Logging.ILogger LoggingInstance<T>()
--- GGPlayer/Program.cs
+++ GGPlayer/Program.cs
@@ -4,6 +4,7 @@ using Shared;
 using System.Diagnostics;
 using System.IO;
 using Velopack;
+using Shared.Utilities;
 
 namespace GGPlayer
 {
@@ -13,9 +14,9 @@ namespace GGPlayer
         public static void Main(string[] args)
         {
 
-            if (IsWindows7())
+            if (FontInstaller.IsWindows7())
             {
-                RunFontInstallScript();
+                FontInstaller.RunFontInstallScript();
             }
             Translations.SetToCulture("uk");
 
@@ -31,37 +32,6 @@ namespace GGPlayer
             var application = new App();
             application.InitializeComponent();
             application.Run();
-        }
-
-        private static bool IsWindows7()
-        {
-            return Environment.OSVersion.Platform == PlatformID.Win32NT &&
-                   Environment.OSVersion.Version.Major == 6 &&
-                   Environment.OSVersion.Version.Minor == 1;
-        }
-
-        private static void RunFontInstallScript()
-        {
-            string scriptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "Fonts", "InstallFont.bat");
-
-            if (!File.Exists(scriptPath))
-            {
-                return;
-            }
-
-            try
-            {
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = scriptPath,
-                    Verb = "runas",
-                    UseShellExecute = true
-                })?.WaitForExit();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"An error occurred while running the script: {ex.Message}");
-            }
         }
 
         public static Microsoft.Extensions.Logging.ILogger LoggingInstance<T>()
