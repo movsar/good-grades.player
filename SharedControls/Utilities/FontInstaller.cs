a36338401b9af4ade01fb0e68631e11e48db3696
FraintSaad <islamqwertyu@mail.ru>
2025-01-06 20:39:14 +0300

    need to check on release version after push to master

--- SharedControls/Utilities/FontInstaller.cs
+++ SharedControls/Utilities/FontInstaller.cs
@@ -10,6 +10,9 @@ namespace Shared.Utilities
 {
     public static class FontInstaller
     {
+        private static readonly string FontsFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts));
+
+        private static readonly string FontFileName = "segmdl2.ttf";
         public static bool IsWindows7()
         {
             return Environment.OSVersion.Platform == PlatformID.Win32NT &&
@@ -17,8 +20,19 @@ namespace Shared.Utilities
                    Environment.OSVersion.Version.Minor == 1;
         }
 
+        public static bool IsFontInstalled()
+        {
+            return File.Exists(Path.Combine(FontsFolderPath, FontFileName));
+        }
+
         public static void RunFontInstallScript()
         {
+            if (IsFontInstalled())
+            {
+                Debug.WriteLine($"The font '{FontFileName}' is already installed.");
+                return;
+            }
+
             string scriptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "Fonts", "InstallFont.bat");
 
             if (!File.Exists(scriptPath))
