5998d1a61b6543482b14bd03ba394e006b084a00
FraintSaad <islamqwertyu@mail.ru>
2025-01-20 21:39:37 +0300

    first commit

--- GGManager/UserControls/SegmentListControl.xaml.cs
+++ GGManager/UserControls/SegmentListControl.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using Shared;
 
 namespace GGManager.UserControls
@@ -15,6 +16,8 @@ namespace GGManager.UserControls
     {
         private readonly ContentStore _contentStore = App.AppHost!.Services.GetRequiredService<ContentStore>();
         public ICommand DeleteSelectedSegment { get; }
+        private Segment? _draggedSegment;
+
         public SegmentListControl()
         {
             InitializeComponent();
@@ -37,7 +40,7 @@ namespace GGManager.UserControls
         private void RedrawSegmentList(string? selectedSegmentId = null)
         {
             lvSegments.Items.Clear();
-            foreach (var segment in _contentStore.DbContext.Segments)
+            foreach (var segment in _contentStore.DbContext.Segments.OrderBy(s => s.Order))
             {
                 lvSegments.Items.Add(segment);
             }
@@ -48,15 +51,24 @@ namespace GGManager.UserControls
                 return;
             }
 
-            var currentSegment = _contentStore.DbContext.Segments.Where(item => item.Id == selectedSegmentId).First();
+            var currentSegment = _contentStore.DbContext.Segments.Where(item => item.Id == selectedSegmentId);
             lvSegments.SelectedItem = currentSegment;
         }
 
         private void BtnNewSection_Click(object sender, RoutedEventArgs e)
         {
-            //создание нового сегмента
-            Segment segment = new Segment() { Title = Translations.GetValue("NewChapter") };
-            //добавление нового сегмента в бд и сохранение
+            // Создание нового сегмента с порядковым номером
+            int maxOrder = _contentStore.DbContext.Segments.Any()
+                ? _contentStore.DbContext.Segments.Max(s => s.Order)
+                : 0;
+
+            Segment segment = new Segment()
+            {
+                Title = Translations.GetValue("NewChapter"),
+                Order = maxOrder + 1
+            };
+
+            // Добавление нового сегмента в БД и сохранение
             _contentStore.DbContext.Add(segment);
             _contentStore.DbContext.SaveChanges();
 
@@ -76,6 +88,102 @@ namespace GGManager.UserControls
 
             _contentStore.SelectedSegment = segment;
         }
+
+        #region Drag-and-Drop
+
+        // клик по элементу списка для начала перетаскивания
+        private void lvSegments_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is not DependencyObject source) return;
+
+            var listViewItem = FindAncestor<ListViewItem>(source);
+            if (listViewItem == null) return;
+
+            _draggedSegment = (Segment?)listViewItem.Content;
+            if (_draggedSegment != null)
+            {
+                DragDrop.DoDragDrop(lvSegments, _draggedSegment, DragDropEffects.Move);
+            }
+        }
+
+        //событие перетаскивания поверх элементов в списке
+        private void lvSegments_DragOver(object sender, DragEventArgs e)
+        {
+            if (_draggedSegment == null) return;
+
+            e.Effects = DragDropEffects.Move;
+
+            // Подсвечиваем элемент, на который наведена мышь
+            if (e.OriginalSource is DependencyObject source)
+            {
+                var targetItem = FindAncestor<ListViewItem>(source);
+                if (targetItem != null)
+                {
+                    targetItem.Background = Brushes.LightBlue;
+                }
+            }
+        }
+
+        //событие, когда мышь покидает элемент
+        private void lvSegments_DragLeave(object sender, DragEventArgs e)
+        {
+            // Убираем подсветку с элемента, с которого ушла мышь
+            if (e.OriginalSource is DependencyObject source)
+            {
+                var targetItem = FindAncestor<ListViewItem>(source);
+                if (targetItem != null)
+                {
+                    targetItem.Background = Brushes.Transparent;
+                }
+            }
+        }
+
+        //событие завершения перетаскивания
+        private void lvSegments_Drop(object sender, DragEventArgs e)
+        {
+            if (_draggedSegment == null) return;
+
+            if (e.OriginalSource is DependencyObject source)
+            {
+                var targetItem = FindAncestor<ListViewItem>(source);
+                if (targetItem != null)
+                {
+                    var targetSegment = (Segment?)targetItem.Content;
+                    if (targetSegment != null && targetSegment != _draggedSegment)
+                    {
+                        SwapOrders(_draggedSegment, targetSegment);
+                        RedrawSegmentList();
+                    }
+                }
+            }
+            _draggedSegment = null; 
+        }
+
+        // Меняет порядок двух сегментов в базе данных
+        private void SwapOrders(Segment draggedSegment, Segment targetSegment)
+        {
+            int tempOrder = draggedSegment.Order;
+            draggedSegment.Order = targetSegment.Order;
+            targetSegment.Order = tempOrder;
+            _contentStore.DbContext.SaveChanges();
+        }
+
+        // Находит родительский элемент заданного типа
+        private static T? FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        {
+            while (current != null)
+            {
+                if (current is T)
+                    return (T)current;
+
+                current = VisualTreeHelper.GetParent(current);
+            }
+
+            return null;
+        }
+
+
+        #endregion
 
         #region Segment Event Handlers
 
@@ -98,7 +206,7 @@ namespace GGManager.UserControls
 
             RedrawSegmentList();
         }
-        #endregion
 
+        #endregion
     }
 }
